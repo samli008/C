7c662d46ece7a522e8d688c56e22c54b5ddc5163
liyang <235938019@qq.com>
2020-09-29 19:28:52 -0400

    liyang

---
+++ 03.constant.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define name "liyang"
+#define name1 "samli008"
+
+int main()
+{
+        printf("my name is %s\n",name);
+        printf("english name is %s\n", name1);
+        return 0;
+}
