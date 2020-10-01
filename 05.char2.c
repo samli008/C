897c3c2ee7c50cd9192b0a2fda2fdaef0e9dcccd
liyang <235938019@qq.com>
2020-09-30 20:18:07 -0400

    liyang

--- 05.char2.c
+++ 05.char2.c
@@ -3,6 +3,13 @@
 int main()
 {
         char a[6]={'l','i','y','a','n','g'};
+        char b[]="samli";
+
         printf("%s\n",a);
+        printf("%s\n",b);
+        printf("%c\n",a[0]);
+        printf("%c\n",b[0]);
+        printf("%c\n",b[4]);
+
         return 0;
 }
