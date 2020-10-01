e35e7937240457fbbf65ab9986984adb597126da
liyang <235938019@qq.com>
2020-09-30 20:09:49 -0400

    liyang

--- 05.char1.c
+++ 05.char1.c
@@ -3,7 +3,12 @@
 int main()
 {
         char a=67,b=99,c='C',d='c';
+        unsigned char height;
+        height=165;
+
         printf("%c%c\n",a,b);
         printf("%c%c\n",c,d);
+        printf("%d\n",height);
+
         return 0;
 }
---
+++ 05.char2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+        char a[6]={'l','i','y','a','n','g'};
+        printf("%s\n",a);
+        return 0;
+}
