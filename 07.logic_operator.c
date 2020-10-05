3f3dca8c6e971106c52e68427150b6e8d5674838
liyang <235938019@qq.com>
2020-10-05 19:26:53 -0400

    liyang

---
+++ 07.logic_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+        int a=5,b=3;
+
+        printf("%d\n",1<2);
+        printf("%d\n",a>b);
+        printf("%d\n",a<=b+1);
+        printf("%d\n",'a'+'b'<='c');
+        printf("%d\n",(a=3)>(b=5));
+
+        return 0;
+}
